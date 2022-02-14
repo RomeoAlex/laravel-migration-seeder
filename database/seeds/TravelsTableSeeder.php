af57388bf4fac38b62860a3a1ef4c3fd54748883
RomeoAlex <romeo.alessandro17@gmail.com>
2022-02-14 23:51:03 +0100

    creato seeder con Faker di Zaninotto

--- database/seeds/TravelsTableSeeder.php
+++ database/seeds/TravelsTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
-
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
-
+//richiamo il model
+use App\Travel;
 class TravelsTableSeeder extends Seeder
 {
     /**
@@ -9,8 +10,20 @@ class TravelsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        //Creo x numero di viaggi nella tabella tramite il model
+        for ($i = 0 ; $i < 10; $i++) { 
+            $new_travel = new Travel();
+            $new_travel->city = $faker->city();
+            $new_travel->type = $faker->$faker->words();
+            $new_travel->days_duration = $faker->$faker->randomDigit();
+            $new_travel->departure_at = $faker->$faker->dateTime();
+            $new_travel->description = $faker->$faker->sentence();
+            $new_travel->available= rand(0,1);
+            $new_travel->timestamps(); 
+            $new_travel->price =$faker->randomFloat(2);
+        }
+        
     }
 }
