5a3ee342972e49a0f651b4cc401694be4a3eae55
RomeoAlex <romeo.alessandro17@gmail.com>
2022-02-14 23:52:50 +0100

    correzione

--- database/seeds/TravelsTableSeeder.php
+++ database/seeds/TravelsTableSeeder.php
@@ -16,10 +16,10 @@ class TravelsTableSeeder extends Seeder
         for ($i = 0 ; $i < 10; $i++) { 
             $new_travel = new Travel();
             $new_travel->city = $faker->city();
-            $new_travel->type = $faker->$faker->words();
-            $new_travel->days_duration = $faker->$faker->randomDigit();
-            $new_travel->departure_at = $faker->$faker->dateTime();
-            $new_travel->description = $faker->$faker->sentence();
+            $new_travel->type = $faker->words();
+            $new_travel->days_duration = $faker->randomDigit();
+            $new_travel->departure_at = $faker->dateTime();
+            $new_travel->description = $faker->sentence();
             $new_travel->available= rand(0,1);
             $new_travel->timestamps(); 
             $new_travel->price =$faker->randomFloat(2);
